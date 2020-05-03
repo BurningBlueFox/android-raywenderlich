bf8fee4d62c5a0717f7f34f66befa63eacdf2d69
Thiago Gorgulho Leão <burningbluefox@gmail.com>
2020-05-03 20:54:07 -0300

    Finish Color My View

--- AboutMe/app/src/main/java/com/thiagogamedev/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/thiagogamedev/aboutme/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.thiagogamedev.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.thiagogamedev.aboutme.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName =
+        MyName("Thiago")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+        //findViewById<Button>(R.id.done_button).setOnClickListener{
+        //   addNickname(it)
+        //}
+
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+
+        binding.apply {
+            //nickname_text.text = nickname_edit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nickname_edit.visibility = View.GONE
+            view.visibility = View.GONE
+            nickname_text.visibility = View.VISIBLE
+        }
+
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
